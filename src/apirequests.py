12576986f8a40ee575bf0f721a2e7ac63f0f852f
voigtstefan <s.voigt@gmx.net>
2017-11-04 13:26:39 +0100

    Working example to use the API

---
+++ src/apirequests.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  4 13:18:52 2017
+
+@author: stefan
+"""
+
+import json
+import requests
+
+"  Get all available asset pairs
+response = requests.get("https://hft-service-dev.lykkex.net/api/AssetPairs").json()
+
+"  Check if API is working
+requests.get("https://hft-service-dev.lykkex.net/api/IsAlive").json()
+
+"  Check if API is working
+response = requests.get("https://hft-service-dev.lykkex.net/api/OrderBooks/BTCUSD")
+
+" Get order book (BTCUSD)
+ob = requests.get("https://hft-service-dev.lykkex.net/api/OrderBooks/BTCUSD").json()
