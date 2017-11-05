52442ce99c4143e2400c5cda550f0034a3a2a250
Paul Pfeiffer <pfeifferpaul90@gmail.com>
2017-11-05 09:32:07 +0100

    implemented fake db by circular buffer

--- src/services/db_service.py
+++ src/services/db_service.py
@@ -1,16 +1,22 @@
+import collections
 import logging as log
-
 import numpy as np
 
-
 class DBService(object):
+    TIME_STAMP_FIELD = 'time_stamp'
+    PRICE_FIELD = 'price'
+
     def make_entry(self, time_stamp, price):
         log.info("Write time {} and price {} to database.".format(time_stamp, price))
+        self.fake_db.append(price)
         pass
 
     def get_price_list(self):
         log.info("Retrieve list of prices from database.")
-        return np.random.uniform(0, 100, 100)
+        return np.array(self.fake_db)
 
     def __init__(self):
+        limit_of_price = 10 ** 6
+        self.fake_db = collections.deque(maxlen=limit_of_price)
+
         log.info("Initialize database service.")
