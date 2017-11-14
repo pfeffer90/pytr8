fdc3ca5d39bbf5cc4d5d6c750d0ecf8248a93563
voigtstefan <s.voigt@gmx.net>
2017-11-14 20:18:42 +0100

    Added latency variable (which may be used to stop trading if API is lagging)

--- src/services/price_service.py
+++ src/services/price_service.py
@@ -1,6 +1,6 @@
 import logging as log
 import time
-
+import datetime
 import requests
 
 
@@ -15,13 +15,22 @@ class PriceService(object):
         order_books = requests.get(PriceService.REQUEST_ORDER_BOOKS_ADDR + PriceService.ASSET_PAIR).json()
         price = self.get_asset_price(order_books)
         volume = self.get_asset_trading_volume(order_books)
+        time_ob = self.get_time(order_books)
+        time_delta = (datetime.datetime.strptime(time_stamp, '%a %b %d %H:%M:%S %Y')-time_ob).total_seconds()
+
         log.info("Timestamp: {}".format(time_stamp))
         log.info("Price: {}".format(price))
+        log.info("System latency: {} secs".format(time_delta))
         return time_stamp, price, volume
 
     def get_asset_trading_volume(self, order_books):
         return order_books[1]['Prices'][-1]['Volume']
 
+    def get_time(self,order_books):
+        time_stamp_ob = order_books[1]['Timestamp']
+        val = datetime.datetime.strptime(time_stamp_ob, '%Y-%m-%dT%H:%M:%S.%f')  
+        return val       
+        
     def get_asset_price(self, order_books):
         try:
             price = order_books[1]['Prices'][-1]['Price']
